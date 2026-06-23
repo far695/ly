42a2773470d8f3cdb0cd9b24101da0e4d4af20ee
far695 <yuan13250391883@gmail.com>
2026-06-23 20:53:09 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,27 +1,10 @@
 /** @type {import('next').NextConfig} */
-// 用于为静态资源（如图像、样式表、JavaScript 文件等）设置 URL 前缀
-// 这在将应用部署到自定义域名或 CDN 上时特别有用，因为它允许您将静态资源存储在不同的位置
-let assetPrefix = `/${ly}/`
-
-// 用于为应用设置基础路径
-// 这在将应用部署到子目录下时特别有用，因为它允许您指定应用所在的目录
-let basePath = `/${ly}`
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-  assetPrefix = `/${ly}/`
-  basePath = `/${ly}`
-}
-
 const nextConfig = {
-  assetPrefix,
-  basePath,
-  reactStrictMode: true,
+  output: 'export',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 }
 
-module.exports = nextConfig
+module.exports = nextConfig 
