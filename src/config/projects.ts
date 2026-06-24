4a1da98bea915ce8e9ce3cb10a01744695a8f779
far695 <yuan13250391883@gmail.com>
2026-06-24 16:19:34 +0800

    Update projects.ts

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -7,19 +7,19 @@ export const projectsConfig = {
     {
       title: "腾讯广告",
       description: "国内年轻社交平台，用黑科技和多元玩法与年轻用户深度互动。",
-      href: "https://edgeone.ai/pages/templates?usecase=portfolio",
+      href: "https://far695.github.io/ly/",
       imageUrl: "https://github.com/far695/ly/blob/main/assets/images/project1.png"
     },
     {
       title: "头条&抖音广告",
       description: "大数据今日头条&抖音媒体资源，引领智能移动广告，整合营销体系。",
-      href: "https://edgeone.ai/pages/templates?usecase=portfolio",
+      href: "https://far695.github.io/ly/",
       imageUrl: "https://github.com/far695/ly/blob/main/assets/images/project2.png"
     },
     {
       title: "快手广告",
       description: "广告投放 +电商直播卖货+视频号长效运营，沉浸式原生视频体验，看见更多好内容。",
-      href: "https://edgeone.ai/pages/templates?usecase=portfolio",
+      href: "https://far695.github.io/ly/",
       imageUrl: "https://github.com/far695/ly/blob/main/assets/images/project3.png"
     }
   ]
